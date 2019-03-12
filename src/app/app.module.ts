8fde095ce8172a608e189e86a7b0d35e105f3b0d
up1 <somkiat.p@gmail.com>
2019-03-12 23:04:43 +0700

    Create note component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
   ],
   imports: [
     BrowserModule
